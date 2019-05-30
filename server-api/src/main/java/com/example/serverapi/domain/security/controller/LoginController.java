fbe868fa767e4137d1ff5ee22f05cbd3e69e5f15
ryan.miao <ryan.miao@nf-3.com>
2019-05-30 20:05:00 +0800

    remove

--- server-api/src/main/java/com/example/serverapi/domain/security/controller/LoginController.java
+++ server-api/src/main/java/com/example/serverapi/domain/security/controller/LoginController.java
@@ -6,8 +6,6 @@ import com.example.serverapi.domain.security.utils.TokenUtils;
 import com.example.serverapi.domain.security.vo.UserToken;
 import io.swagger.annotations.ApiParam;
 import javax.servlet.http.HttpServletResponse;
-import org.redisson.api.RMapCache;
-import org.redisson.api.RedissonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,12 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/auth")
 public class LoginController {
 
-    private final RedissonClient redissonClient;
     private final IAuthService authService;
 
-    public LoginController(RedissonClient redissonClient,
+    public LoginController(
         IAuthService authService) {
-        this.redissonClient = redissonClient;
         this.authService = authService;
     }
 
@@ -36,11 +32,6 @@ public class LoginController {
         @ApiParam("password sha1后的值") @RequestParam String password,
         HttpServletResponse response
     ) {
-
-        RMapCache<Object, Object> cache = redissonClient
-            .getMapCache("auth_server_token");
-        cache.put(username, password);
-
         UserToken token = authService.doLogin(username, password);
 
         TokenUtils.writeTokenToCookie(response, token.getToken());
